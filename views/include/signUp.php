eec2dfde0ff3ec2e66ffc23f1c6fa53413a30a0d
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-05-15 21:55:26 +0200

    sign up page

---
+++ views/include/signUp.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <title>sign up</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="d-flex min-vh-100 justify-content-center align-items-center">
+            <form class="col-3">
+                <div class="row text-center">
+                    <h1><strong>Sign up</strong></h1>
+                </div>
+                <div class="row m-2">
+                    <input type="text" class="form-control" id="" placeholder="First name">
+                </div>
+                <div class="row m-2">
+                    <input type="text" class="form-control" id="" placeholder="Last name">
+                </div>
+                <div class="row m-2">
+                    <input type="text" class="form-control" id="" placeholder="Adress">
+                </div>
+                <div class="row m-2">
+                    <input type="email" class="form-control" id="" placeholder="Email">
+                </div>
+                <div class="row m-2">
+                    <input type="password" class="form-control" id="" placeholder="Password">
+                </div>
+                <div class="row m-2">
+                    <button type="submit" class="btn btn-primary">Create new account</button>
+                </div>
+            </form>
+        </div>
+    </div>
