896c95012328e3bb12ebe3d45900fe7321105d52
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-05-15 20:14:01 +0200

    login page

---
+++ views/include/logIn.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="d-flex flex-volom min-vh-100 justify-content-center align-items-center">
+            <form class="col-3">
+                <div class="form-group row m-2 text-center">
+                    <h1><strong>Log in</strong></h1>
+                </div>
+                <div class="form-group row m-2">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+                <div class="form-group row m-2">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+                <div class="form-group row m-2">
+                    <button type="submit" class="btn btn-primary">Log in</button>
+                </div>
+            </form>
+        </div>
+    </div>
