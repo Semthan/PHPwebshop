c4e4c88b972595cc92e029b9073994d1f2f89d0f
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-05-17 10:13:37 +0200

    checkout page

---
+++ views/include/checkout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <title>Check out</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Cart</h1>
+        <div class="row d-flex border">
+            <div class="col-10 border">
+                <img src="pic.PNG" class="img-thumbnail">
+            </div>
+            <div class="col align-self-end">
+                <h2>Price: </h2>
+            </div>
+        </div>
+        <h1>Sum:</h1>
+    </div>
+
+</body>
+
+</html>
