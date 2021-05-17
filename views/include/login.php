5c3b2235355f43bdc25fef849083566702ce989d
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-05-17 11:45:29 +0200

    login page

--- views/include/login.php
+++ views/include/login.php
@@ -1,11 +1,16 @@
-<form action="#" method="POST">   
-    <div>
-        <input type="text" name="email" placeholder="E-mail">
-    </div>
-    <div>
-        <input type="text" name="password" placeholder="Password">
-    </div>
-    <div>
-        <input type="submit" value="Login">
-    </div>
-</form>
+<div class="d-flex min-vh-100 justify-content-center align-items-center">
+    <form class="col-3" action="#" method="POST">
+        <div class="form-group row m-2 text-center">
+            <h1><strong>Log in</strong></h1>
+        </div>
+        <div class="form-group row m-2">
+            <input type="email" name="email" class="form-control" placeholder="Email">
+        </div>
+        <div class="form-group row m-2">
+            <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
+        </div>
+        <div class="form-group row m-2">
+            <input type="submit" value="Login" class="btn btn-primary">
+        </div>
+    </form>
+</div>
