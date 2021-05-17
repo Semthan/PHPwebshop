4e4722fa3f9886cf8595ca0689be84cfa759701d
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-05-17 10:14:07 +0200

    main page

---
+++ views/include/main.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row p-2">
+            <div class="col"></div>
+            <div class="col-8">
+                <div class="row border">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="container">
+                            <a class="navbar-brand" href="#">
+                                <h1>Fakeia</h1>
+                            </a>
+                            <div class="collapse navbar-collapse nav-font">
+                                <ul class="navbar-nav ms-auto">
+                                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                                        <a class="nav-link" href="#">
+                                            <h3>Login</h3>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                                        <a class="nav-link" href="#">
+                                            <h3>Register</h3>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="row">
+                    <div class="bg-light p-5 rounded-lg m-3 border text-center">
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius, dignissimos at quos obcaecati aliquid quibusdam vitae blanditiis eligendi ipsam commodi sapiente totam amet fuga, iusto hic fugiat dolorum reiciendis.</p>
+
+                        <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi illo fugit ex. Unde ducimus labore obcaecati quasi expedita beatae a ab accusantium dolores sapiente, aliquam autem, totam harum quas inventore?</p>
+                        <a class="btn btn-dark btn-lg" href="#" role="button">Browse Shop</a>
+                    </div>
+                </div>
+                <h2 class="bold m-3">Categories</h2>
+                <div class="row border">
+                    <div class="card m-auto" style="width: 18rem;">
+                        <img src="pic.PNG" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                        </div>
+                    </div>
+
+                    <div class="card m-auto" style="width: 18rem;">
+                        <img src="pic.PNG" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                        </div>
+                    </div>
+
+                    <div class="card m-auto" style="width: 18rem;">
+                        <img src="pic.PNG" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</body>
+
+</html>
