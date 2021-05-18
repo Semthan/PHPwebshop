bdb56a8895e247a89b078caf9115ab83ae363f59
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-05-18 14:34:55 +0200

    products page wip

---
+++ views/include/products.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <title>products</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row p-2">
+            <div class="col"></div>
+            <div class="col-8">
+                <div class="row border">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="container">
+                            <a class="navbar-brand" href="#">
+                                <h1>Fakeia</h1>
+                            </a>
+                            <div class="collapse navbar-collapse nav-font">
+                                <ul class="navbar-nav ms-auto">
+                                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                                        <a class="nav-link" href="#">
+                                            <h3>Login</h3>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                                        <a class="nav-link" href="#">
+                                            <h3>Register</h3>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="row border">
+                    another header
+                </div>
+                <div class="row border p-2">
+                    <h3>Categories</h3>
+                    <div class="col-3 border">
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                    </div>
+                    <div class="col border">
+                        <div class="row">
+                            <div class="card m-auto" style="width: 18rem;">
+                                <img src="pic.PNG" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">Card title</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</body>
+
+</html>
