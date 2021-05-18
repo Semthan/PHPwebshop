4856acf48156e074f5242fc942a843f16980da31
Kevin Basele <kevinbasele@hotmail.se>
2021-05-18 11:30:10 +0200

    crud admin

--- models/ProductModel.php
+++ models/ProductModel.php
@@ -9,9 +9,9 @@
 
         public function deleteProduct($productId){
             
-            $statement = "DELETE * FROM products WHERE product_id = :id";
+            $statement = "DELETE FROM products WHERE product_id = :id";
             $this->db->delete($statement, ["id"=>$productId]);
-            header('Location: "https://localhost/webb20/PHPwebshop/index.php?page=admin"');
+            header('Location: index.php?page=admin');
         }
 
         public function fetchAllProducts()
